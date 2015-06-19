a8638041d84f1b2a86e1fd9913e98cd3b75c1b0a
ajayi1987 <ajayi1987@me.com>
2015-06-18 21:02:40 -0500

    Ex- 17 Conditional Statements

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,21 @@ $("#submit").on("click", function() {
 	console.log(comment);
 	$("#visible-comment").html(comment);
 });
+
+$("#textarea").on("keyup", function() {
+	console.log("keyup just happened!!");
+	var nameName = 3;
+	var name ="string";
+	var charCount = $("#textarea").val().length; //length of the content of the textarea to a variable
+	console.log(charCount);
+	$("#char-count").html(charCount);
+	if(charCount > 50) {
+		$("#char-count").css("color", "red");
+	} else {
+		$("#char-count").css("color", "yellow");
+
+	};
+});
+
+
+
