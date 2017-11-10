59469c473c72fff8ecb70bc9a5557d5f6982ba72
Ajayi Agbebaku <ajayi1987@me.com>
2017-11-10 18:02:27 +0000

    update oct 2017

--- program4.rb
+++
@@ -1,5 +0,0 @@
-(0...10).each do |n|
-#while (number <= 10) do #while this condition is true, do...
-	p "the new number is #{n}" #whatever is in this code block
-	#number += 1 # short for number = number +1
-end # don't forget your end
